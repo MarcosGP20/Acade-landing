c4392f06049ceddd2967705c60fa45d3d842de57
MarcosGP20 <marcospinto2003@gmail.com>
2025-03-25 21:04:57 -0300

    creacion archivo jugadores

---
+++ script.js
@@ -0,0 +1,102 @@
+const jugadores = [
+  {
+    nombre: "Gabriel Arias",
+    posicion: "Arquero",
+    numero: 21,
+    imagen:
+      "https://www.racingclub.com.ar/img/futbol/plantel/thumbs/880_arias.jpg?v=2",
+  },
+  {
+    nombre: "Facundo Cambeses",
+    posicion: "Arquero",
+    numero: 25,
+    imagen:
+      "https://www.racingclub.com.ar/img/futbol/plantel/thumbs/1583_cambeses.jpg?v=2",
+  },
+  { nombre: "Agustín García Basso", posicion: "Defensor", numero: 2, imagen },
+  { nombre: "Marco Di Cesare", posicion: "Defensor", numero: 3, imagen },
+  {
+    nombre: "Gastón Martirena",
+    posicion: "Defensor",
+    numero: 15,
+    imagen:
+      "https://www.racingclub.com.ar/img/futbol/plantel/thumbs/1566_martirena.jpg?v=2",
+  },
+  { nombre: "Ignacio Rodríguez", posicion: "Defensor", numero: 19, imagen },
+  { nombre: "Germán Conti", posicion: "Defensor", numero: 20, imagen },
+  {
+    nombre: "Nazareno Colombo",
+    posicion: "Defensor",
+    numero: 23,
+    imagen:
+      "https://www.racingclub.com.ar/img/futbol/plantel/thumbs/1568_colombo.jpg?v=2",
+  },
+  {
+    nombre: "Gabriel Rojas",
+    posicion: "Defensor",
+    numero: 27,
+    imagen:
+      "https://www.racingclub.com.ar/img/futbol/plantel/thumbs/1398_rojas.jpg?v=2",
+  },
+  {
+    nombre: "Facundo Mura",
+    posicion: "Defensor",
+    numero: 34,
+    imagen:
+      "https://www.racingclub.com.ar/img/futbol/plantel/thumbs/1216_mura.jpg?v=2",
+  },
+  {
+    nombre: "Juan Ignacio Nardoni",
+    posicion: "Mediocampista",
+    numero: 5,
+    imagen:
+      "https://www.racingclub.com.ar/img/futbol/plantel/thumbs/1385_nardoni.jpg?v=2",
+  },
+  {
+    nombre: "Matías Zaracho",
+    posicion: "Mediocampista",
+    numero: 11,
+    imagen:
+      "https://www.racingclub.com.ar/img/futbol/plantel/thumbs/668_zaracho.jpg?v=2",
+  },
+  { nombre: "Santiago Sosa", posicion: "Mediocampista", numero: 13, imagen },
+  { nombre: "Martín Barrios", posicion: "Mediocampista", numero: 16, imagen },
+  { nombre: "Richard Sánchez", posicion: "Mediocampista", numero: 26, imagen },
+  {
+    nombre: "Agustín Almendra",
+    posicion: "Mediocampista",
+    numero: 32,
+    imagen:
+      "https://www.racingclub.com.ar/img/futbol/plantel/thumbs/1565_almendra.jpg?v=2",
+  },
+  {
+    nombre: "Bruno Zuculini",
+    posicion: "Mediocampista",
+    numero: 36,
+    imagen:
+      "https://www.racingclub.com.ar/img/futbol/plantel/thumbs/31_zuculini.jpg?v=2",
+  },
+  { nombre: "Maximiliano Salas", posicion: "Delantero", numero: 7, imagen },
+  {
+    nombre: "Adrián Martínez",
+    posicion: "Delantero",
+    numero: 9,
+    imagen:
+      "https://www.racingclub.com.ar/img/futbol/plantel/thumbs/1586_martinez.jpg?v=2",
+  },
+  {
+    nombre: "Luciano Vietto",
+    posicion: "Delantero",
+    numero: 10,
+    imagen:
+      "https://www.racingclub.com.ar/img/futbol/plantel/thumbs/29_vietto.jpg?v=2",
+  },
+  {
+    nombre: "Santiago Solari",
+    posicion: "Delantero",
+    numero: 28,
+    imagen:
+      "https://www.racingclub.com.ar/img/futbol/plantel/thumbs/1585_solari.jpg?v=2",
+  },
+  { nombre: "Adrián Balboa", posicion: "Delantero", numero: 77, imagen },
+];
